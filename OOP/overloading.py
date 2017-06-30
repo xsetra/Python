4c4d32169487de768acb6f991269fa7ccc7d59c5
Abdullah ÇALIŞKAN <abdullahcaliskan511@gmail.com>
2017-06-30 18:58:14 +0300

    Overloading

--- OOP/overloading.py
+++ OOP/overloading.py
@@ -1,16 +1,25 @@
+# -*- coding: utf-8 -*-
 class Person:
 	def __init__(self, first, last, age):
 		self.firstname = first
 		self.lastname = last
 		self.age = age
 
-	def get_names(self):
-		return self.firstname + " " + self.lastname
-
 	def get_names(self, age=None):
-		return self.firstname + " " + self.lastname + " , " + str(self.age)
+		if age:
+			return self.firstname + " " + self.lastname + " , " + str(self.age)
+		else:
+			return self.firstname + " " + self.lastname
 
 
-""" Python overloading işlemini zaten yapıyor diyebiliriz. Function family olusturuyor
-Yani int, double, float halleri için zaten metodlar var.
+if __name__ == "__main__":
+	x = Person("Abdullah", "Caliskan", 20)
+	print(x.get_names())
+	print(x.get_names(1))
+
+""" 
+#  overloading işlemini zaten yapıyor diyebiliriz. Function family olusturuyor
+# Yani int, double, float halleri için zaten metodlar var.
+#1 argümanlı bir metod ve argümansız çalışacak metotlar yapacaksanız;
+#argümanı isteğe bağlı hale getirmelisiniz. yada *args kullanın.
 """
